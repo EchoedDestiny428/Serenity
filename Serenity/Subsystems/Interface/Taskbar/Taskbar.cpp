ae357d52d98e5102c0459a229307c6f6b8cdc5d9
echoeddestiny428 <zacharyng428@gmail.com>
2026-07-06 10:27:23 +0800

    feat: styling

--- Serenity/Subsystems/Interface/Taskbar/Taskbar.cpp
+++ Serenity/Subsystems/Interface/Taskbar/Taskbar.cpp
@@ -1,36 +1,29 @@
 #include "Taskbar.h"
-#include "Graphics/GraphicsEngine.h"
-#include "Windowing/OverlayEngine.h"
+#include "../../Graphics/GraphicsEngine.h"
+#include "../../Windowing/OverlayEngine.h"
 #include <d2d1.h>
 #include <dwmapi.h>
 
 #pragma comment(lib, "dwmapi.lib")
 
-extern void __stdcall SetupModernBlur(HWND hWnd);
-
 static HWND hTaskbarWnd = nullptr;
 static int taskbarHeight = 56;
 static int taskbarWidth = 600;
-static int margin = 12;
 
-// Animation States
+static int dockMargin = 32; 
+
 static bool g_isVisible = false;
 static bool g_isAnimating = false;
-
-#ifndef DWMWA_WINDOW_CORNER_PREFERENCE
-#define DWMWA_WINDOW_CORNER_PREFERENCE 33
-#define DWMWCP_ROUND 2
-#endif
 
 void SlideTaskbar(HWND hWnd, bool slideUp) {
     g_isAnimating = true;
 
     int screenH = GetSystemMetrics(SM_CYSCREEN);
-    int visibleY = screenH - taskbarHeight - margin;
+    int visibleY = screenH - taskbarHeight - dockMargin;
     int hiddenY = screenH + 10;
 
     ULONGLONG startTime = GetTickCount64();
-    float duration = 400.0f;
+    float duration = 300.0f;
 
     RECT rc;
     GetWindowRect(hWnd, &rc);
@@ -53,13 +46,13 @@ void SlideTaskbar(HWND hWnd, bool slideUp) {
         if (slideUp) {
             float inv = 1.0f - t;
             eased = 1.0f - (inv * inv * inv);
-        }
-        else {
+        } else {
             eased = t * t * t;
         }
 
         int currentY = (int)(startY + (endY - startY) * eased);
-        SetWindowPos(hWnd, NULL, rc.left, currentY, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_NOACTIVATE);
+        
+        SetWindowPos(hWnd, HWND_TOPMOST, rc.left, currentY, 0, 0, SWP_NOSIZE | SWP_NOACTIVATE);
 
         DwmFlush();
     }
@@ -67,9 +60,12 @@ void SlideTaskbar(HWND hWnd, bool slideUp) {
     g_isAnimating = false;
 }
 
-
 LRESULT CALLBACK Taskbar_WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
+        case WM_ERASEBKGND: {
+            return 1; 
+        }
+
         case WM_PAINT: {
             PAINTSTRUCT ps;
             BeginPaint(hWnd, &ps);
@@ -78,7 +74,27 @@ LRESULT CALLBACK Taskbar_WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPa
             if (target) {
                 target->BeginDraw();
 
-                target->Clear(D2D1::ColorF(0.1f, 0.1f, 0.15f, 0.5f));
+                target->Clear(D2D1::ColorF(0.0f, 0.0f, 0.0f, 0.0f));
+
+                ID2D1SolidColorBrush* bgBrush = nullptr;
+                ID2D1SolidColorBrush* borderBrush = nullptr;
+
+                target->CreateSolidColorBrush(D2D1::ColorF(0.08f, 0.08f, 0.08f, 0.8f), &bgBrush);
+                target->CreateSolidColorBrush(D2D1::ColorF(0.7f, 0.3f, 1.0f, 1.0f), &borderBrush);
+
+                if (bgBrush && borderBrush) {
+                    D2D1_SIZE_F size = target->GetSize();
+
+                    D2D1_ROUNDED_RECT rr = D2D1::RoundedRect(
+                        D2D1::RectF(1.0f, 1.0f, size.width - 1.0f, size.height - 1.0f),
+                        12.0f, 12.0f
+                    );
+
+                    target->FillRoundedRectangle(&rr, bgBrush);
+                    target->DrawRoundedRectangle(&rr, borderBrush, 1.0f);
+                    bgBrush->Release();
+                    borderBrush->Release();
+                }
 
                 // TODO: DrawTaskbarIcons() goes here
 
@@ -101,14 +117,15 @@ LRESULT CALLBACK Taskbar_WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 
                 int left = (screenW - taskbarWidth) / 2;
                 int right = left + taskbarWidth;
-                int visibleY = screenH - taskbarHeight - margin;
+                int visibleY = screenH - taskbarHeight - dockMargin;
                 
-                int triggerTop = visibleY - 20; 
+                int triggerTop = visibleY - 40; 
 
                 bool inZone = (pt.x >= left && pt.x <= right && pt.y >= triggerTop);
 
                 if (inZone && !g_isVisible) {
                     g_isVisible = true;
+                    SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
                     SlideTaskbar(hWnd, true);
                 }
                 else if (!inZone && g_isVisible) {
@@ -147,24 +164,19 @@ void Taskbar_Init(HINSTANCE hInstance) {
     settings.y = startY;
     settings.opacity = 255;
 
-    settings.styleCallback = SetupModernBlur;
-
     hTaskbarWnd = OverlayEngine_Create(hInstance, settings, Taskbar_WndProc);
 
-    SetWindowLong(hTaskbarWnd, GWL_EXSTYLE, GetWindowLong(hTaskbarWnd, GWL_EXSTYLE) & ~WS_EX_TRANSPARENT);
+    SetWindowLong(hTaskbarWnd, GWL_EXSTYLE, GetWindowLong(hTaskbarWnd, GWL_EXSTYLE) & ~(WS_EX_TRANSPARENT | WS_EX_LAYERED));
 
-    int cornerPreference = DWMWCP_ROUND;
-    DwmSetWindowAttribute(hTaskbarWnd, DWMWA_WINDOW_CORNER_PREFERENCE, &cornerPreference, sizeof(cornerPreference));
+    MARGINS margins = { -1, -1, -1, -1 };
+    DwmExtendFrameIntoClientArea(hTaskbarWnd, &margins);
 }
 
 void Taskbar_Show() {
     if (hTaskbarWnd) {
         Graphics::Initialize(hTaskbarWnd);
-        SetupModernBlur(hTaskbarWnd);
         ShowWindow(hTaskbarWnd, SW_SHOWNA);
-
         SetTimer(hTaskbarWnd, 1, 16, NULL);
-
         InvalidateRect(hTaskbarWnd, NULL, FALSE);
     }
 }
