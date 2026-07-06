4a9fc7c60bc6bea96ac3a18dd2e4b5ef3d899983
echoeddestiny428 <zacharyng428@gmail.com>
2026-07-06 10:48:11 +0800

    refactor: imports

--- Serenity/Subsystems/Interface/Taskbar/Taskbar.cpp
+++ Serenity/Subsystems/Interface/Taskbar/Taskbar.cpp
@@ -1,6 +1,6 @@
 #include "Taskbar.h"
-#include "../../Graphics/GraphicsEngine.h"
-#include "../../Windowing/OverlayEngine.h"
+#include "Graphics/GraphicsEngine.h"
+#include "Windowing/OverlayEngine.h"
 #include <d2d1.h>
 #include <dwmapi.h>
 
